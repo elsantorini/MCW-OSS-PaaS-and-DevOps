3f33c55edd48e334022f4145a068776287c11743
Ubuntu <elsantor@microsoft.com>
2019-06-11 13:22:40 +0000

    Added Docker configuration

--- Hands-on lab/lab-files/app.js
+++ Hands-on lab/lab-files/app.js
@@ -13,7 +13,7 @@ var userSession = require('./routes/session');
 
 var app = express();
 
-var databaseUrl = 'mongodb://localhost:27017/best-for-you-organics';
+var databaseUrl = 'mongodb://labcosmoselsantor:zDTf3SYocSzkdTBa1sJDp5n2vMcrcBNoq7axkv2ScrxYtGOddCb3iNuLJkOtvmdIGPpLw6TLXX5gzEfZ5jI9Fw==@labcosmoselsantor.documents.azure.com:10255/best-for-you-organics?ssl=true&replicaSet=globaldb';
 
 var mongoose = require('mongoose');
 mongoose.Promise = require('bluebird');
